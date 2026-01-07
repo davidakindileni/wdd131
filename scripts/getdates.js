7637cd477adfd440b3814ab775aa4920e5148112
davidakindileni <davidakindileni@yahoo.co.uk>
2026-01-07 11:49:59 +0100

    w01 home page

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+// 26T1
+//
+// Set the year for copyright
+document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+
+// Set the date when last updated
+let a = document.lastModified;
+document.getElementById("lastmodified").innerHTML = a;
